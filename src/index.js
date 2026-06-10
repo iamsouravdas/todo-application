2da3ab456a24d3af0b418711701a9fd114432050
SOURAV DAS <iamsouravdasofficial@gmail.com>
2026-06-10 19:15:19 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT ?? 8000;
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello from the Server'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('App is listening at PORT: ', PORT);
+});
